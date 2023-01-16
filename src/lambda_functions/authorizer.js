946a0cb5d210eef494b95637f2400b4896176938
Seungho <poream3387@gmail.com>
2023-01-16 14:17:06 +0900

    FIXING

---
+++ src/lambda_functions/authorizer.js
@@ -0,0 +1,54 @@
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.handler = function (event, context, callback) {
+    var jwt = event.authorizationToken; // Header needs to be "Authorization" and when retrieving it has to be authorizationToken
+
+    admin
+        .auth()
+        .verifyIdToken(jwt)
+        .then((response) => {
+            callback(null, generatePolicy("user", "Allow", event.methodArn));
+            // callback(null, generatePolicy('user', 'Deny', event.methodArn));
+        })
+        .catch((e) => {
+            callback(null, generatePolicy("user", "Deny", event.methodArn));
+        });
+
+    // // TEST CODE
+    // if (jwt == "DONE") {
+    //     callback(null, generatePolicy("user", "Allow", event.methodArn));
+    // } else {
+    //     callback(null, generatePolicy("user", "Deny", event.methodArn));
+    // }
+};
+
+// Help function to generate an IAM policy
+var generatePolicy = function (principalId, effect, resource) {
+    var authResponse = {};
+
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+        let policyDocument = {};
+        policyDocument.Version = "2012-10-17";
+        policyDocument.Statement = [];
+
+        let statementOne = {};
+        statementOne.Action = "execute-api:Invoke";
+        statementOne.Effect = effect;
+        statementOne.Resource = resource;
+
+        policyDocument.Statement[0] = statementOne;
+        authResponse.policyDocument = policyDocument;
+    }
+
+    console.log(authResponse);
+
+    // Optional output with custom properties of the String, Number or Boolean type.
+    // authResponse.context = {
+    //     stringKey: "Success!",
+    //     numberKey: 123,
+    //     booleanKey: true,
+    // };
+    return authResponse;
+};
